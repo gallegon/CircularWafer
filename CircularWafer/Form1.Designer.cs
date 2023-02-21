c2008940c93912c754549d809559bdc18ce9447c
Nicholai Gallegos <gallegon@oregonstate.edu>
2023-02-21 08:30:35 -0800

    added circular wafer solution to repo

---
+++ CircularWafer/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CircularWafer
+{
+    partial class WaferPoints
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.point_count = new System.Windows.Forms.TextBox();
+            this.draw_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // point_count
+            // 
+            this.point_count.HideSelection = false;
+            this.point_count.Location = new System.Drawing.Point(15, 348);
+            this.point_count.Name = "point_count";
+            this.point_count.Size = new System.Drawing.Size(212, 20);
+            this.point_count.TabIndex = 0;
+            // 
+            // draw_button
+            // 
+            this.draw_button.Location = new System.Drawing.Point(15, 374);
+            this.draw_button.Name = "draw_button";
+            this.draw_button.Size = new System.Drawing.Size(75, 23);
+            this.draw_button.TabIndex = 1;
+            this.draw_button.Text = "Draw Points";
+            this.draw_button.UseVisualStyleBackColor = true;
+            this.draw_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter number of points below:";
+            // 
+            // WaferPoints
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 405);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.draw_button);
+            this.Controls.Add(this.point_count);
+            this.Name = "WaferPoints";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox point_count;
+        private System.Windows.Forms.Button draw_button;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CircularWafer/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CircularWafer
+{
+    public partial class WaferPoints : Form
+    {
+        public WaferPoints()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Parse textbox to in as the number of points to draw
+            int num_points = Int32.Parse(point_count.Text);
+
+            int radius = 150;  // radius of the wafer
+            int padding = 3;  // padding to keep points from being on edge of wafer
+            int spokes = 8;  // number of radial spokes to place points along
+
+            // calculate the number of concentric circles
+            int ring_counter = 1;
+            double num_rings = (double)num_points / (double)spokes;
+            double radius_interval = (double)(radius - padding) / num_rings;
+
+
+            double current_radius = 0;
+            double Tau = 6.2831853071795862;
+
+            double angle_slice = Tau / (double) spokes;
+
+            Graphics g = this.CreateGraphics();
+            
+            // Clear the drawing
+            g.Clear(Color.AntiqueWhite);
+
+            // Create the pens for drawing dots and circles
+            Pen red_pen = new Pen(Color.Red, 3);
+            Pen black_pen = new Pen(Color.Black, 1);
+
+            // Draw the circle and center
+            g.DrawEllipse(black_pen, 0, 0, 300, 300); ;
+            g.DrawEllipse(red_pen, 150, 150, 3, 3);
+
+            // While there are still points to be drawn, place points and draw circles
+            while (num_points > 1) {
+                current_radius = ring_counter * radius_interval;
+                float offset = 150 - (float)current_radius;
+                float current_diameter = 2 * (float)current_radius;
+                g.DrawEllipse(black_pen, offset, offset, current_diameter, current_diameter);
+
+                // This is the "meat" of the algorithm, draw points along a single radius, 
+                // depending on the number of radial spokes
+                for (int i = 0; i < spokes; ++i) {
+                    if (num_points == 1) {
+                        break;
+                    }
+
+                    // This converts polar to cartesian coordiantes
+                    double theta = angle_slice * i;
+                    double x = current_radius * Math.Cos(theta);
+                    double y = current_radius * Math.Sin(theta);
+
+                    // offset the xy coordinates so the full circle is draw to the window
+                    x += 150;
+                    y += 150;
+                    
+                    // Draw the "point" as a small circle and decrement the number of points to draw
+                    g.DrawEllipse(red_pen, (float) x, (float) y, 3, 3);
+                    num_points--;
+                }
+                // increment the ring level
+                ring_counter++;
+            }
+        }
+    }
+}
